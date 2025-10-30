7a368875908568a6febc3debcd114408b9ece048
Thazmania1 <martinezpicazoian@gmail.com>
2025-10-30 10:07:24 +0100

    Chunk rework

--- Assets/Scripts/Mapping/Editor/ChunkBehaviourEditor.cs
+++ Assets/Scripts/Mapping/Editor/ChunkBehaviourEditor.cs
@@ -4,37 +4,87 @@ using UnityEngine;
 using static ChunkBehaviour;
 
 [CustomEditor(typeof(ChunkBehaviour))]
+[CanEditMultipleObjects]
 public class ChunkBehaviourEditor : Editor
 {
     public override void OnInspectorGUI()
     {
         // Sets up serliazed fields
-        ChunkBehaviour chunkManager = (ChunkBehaviour)target;
+        ChunkBehaviour chunkBehaviour = (ChunkBehaviour)target; // Only used in single selection cases
 
-        serializedObject.Update();
-
-        SerializedProperty
-            concatenatingPosition = serializedObject.FindProperty(chunkManager.ConcatenatingPositionReference);
-
-        EditorGUILayout.LabelField("Concatenating position");
-        EditorGUI.indentLevel++;
-        EditorGUILayout.PropertyField(concatenatingPosition.FindPropertyRelative("x"), new GUIContent("X"));
-        EditorGUILayout.PropertyField(concatenatingPosition.FindPropertyRelative("y"), new GUIContent("Z"));
-        EditorGUI.indentLevel--;
-        EditorGUILayout.Space();
-
-        // Layer creation logic
-        if(GUILayout.Button("Generate new layer"))
+        // Wouldn't make sense to allow groupal position changes or layer generation
+        if(targets.Length > 1)
         {
-            // Creates the empty layer object
-            GameObject newLayer = new GameObject();
-            Undo.RegisterCreatedObjectUndo(newLayer, "Generated new layer");
-            newLayer.transform.parent = chunkManager.transform;
-            newLayer.isStatic = true;
-            newLayer.AddComponent<LayerBehaviour>().TranslateHeightPosition();
+            EditorGUILayout.HelpBox("Multi-chunk position editing and layer generation is not allowed.", MessageType.Info);
+        }
+        else
+        {
+            serializedObject.Update();
+
+            SerializedProperty
+                concatenatingPosition = serializedObject.FindProperty(chunkBehaviour.ConcatenatingPositionReference);
+
+            EditorGUILayout.LabelField("Concatenating position");
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(concatenatingPosition.FindPropertyRelative("x"), new GUIContent("X"));
+            EditorGUILayout.PropertyField(concatenatingPosition.FindPropertyRelative("y"), new GUIContent("Z"));
+            EditorGUI.indentLevel--;
+            EditorGUILayout.Space();
+
+            // Layer creation logic
+            if (GUILayout.Button("Generate new layer"))
+            {
+                // Creates the empty layer object
+                GameObject newLayer = new GameObject();
+                Undo.RegisterCreatedObjectUndo(newLayer, "Generated new layer");
+                newLayer.transform.parent = chunkBehaviour.transform;
+                newLayer.isStatic = true;
+                newLayer.AddComponent<LayerBehaviour>().TranslateHeightPosition();
+            }
+
+            serializedObject.ApplyModifiedProperties();
+            chunkBehaviour.TranslateConcatenatingPosition();
         }
 
-        serializedObject.ApplyModifiedProperties();
-        chunkManager.TranslateConcatenatingPosition();
+        EditorGUILayout.Space();
+
+        // Layer sort logic
+        if(GUILayout.Button("Sort layers ascendingly"))
+        {
+            foreach(var gameObject in targets)
+            {
+                ChunkBehaviour isolatedChunkBehaviour = gameObject as ChunkBehaviour;
+                if (isolatedChunkBehaviour == null) continue;
+
+                // Collects valid LayerBehaviour children
+                List<LayerBehaviour> layers = new List<LayerBehaviour>();
+                HashSet<float> seenLayers = new HashSet<float>();
+
+                // Tracks duplicate layer heights
+                List<LayerBehaviour> duplicatedLayers = new List<LayerBehaviour>();
+
+                foreach (Transform child in isolatedChunkBehaviour.transform)
+                {
+                    LayerBehaviour layer = child.GetComponent<LayerBehaviour>();
+                    if (layer == null) continue;
+
+                    if (seenLayers.Contains(layer.Height))
+                    {
+                        duplicatedLayers.Add(layer);
+                        continue;
+                    }
+
+                    seenLayers.Add(layer.Height);
+                    layers.Add(layer);
+                }
+
+                // Destroys all duplicate layers
+                foreach(LayerBehaviour duplicatedLayer in duplicatedLayers) DestroyImmediate(duplicatedLayer.gameObject);
+
+                // Sort layers in the hierarchy
+                layers.Sort((a, b) => a.Height.CompareTo(b.Height));
+                for(int i = 0; i < layers.Count; i++) layers[i].transform.SetSiblingIndex(i);
+            }
+        }
     }
 }
--- Assets/Scripts/Movement/PieceMovement.cs
+++ Assets/Scripts/Movement/PieceMovement.cs
@@ -1,6 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+// Used for individual piece movement patterns
 public class PieceMovement : MonoBehaviour
 {
-    
+    // Tracks the piece's current chunk, layer, and block position
+    protected ChunkBehaviour _currentChunk;
+    protected LayerBehaviour _currentLayer;
+    protected int _currentBlock;
+
+    // Tracks if the piece is currently moving
+    protected bool _isMoving = false;
+
+    private void Start()
+    {
+        UpdatePiecePosition();
+        CalculatePieceMoves();
+    }
+
+    public void UpdatePiecePosition()
+    {
+        _currentChunk = transform.root.GetComponent<ChunkBehaviour>();
+        _currentLayer = transform.parent.parent.GetComponent<LayerBehaviour>();
+        _currentBlock = transform.parent.GetSiblingIndex();
+    }
+
+    // The piece's movement possibilities
+    public void CalculatePieceMoves()
+    {
+        
+    }
 }
