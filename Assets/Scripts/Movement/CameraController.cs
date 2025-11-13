809ed9b5ca6d5c61119622a426b19590d2447964
Thazmania1 <martinezpicazoian@gmail.com>
2025-11-13 09:55:00 +0100

    Camera controller

---
+++ Assets/Scripts/Movement/CameraController.cs
@@ -0,0 +1,31 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Tracks the focused piece
+    [SerializeField] private Transform _target;
+
+    // Camera properties
+    private float _distance = -10f;
+    private float _speed = 5f;
+    private Vector2 _orbitAngles = new Vector2(-33f, -33f);
+
+    void Update()
+    {
+        // Right click and drag to rotate around the focused piece
+        if(Input.GetMouseButton(1))
+        {
+            _orbitAngles.x += Input.GetAxis("Mouse Y") * _speed;
+            _orbitAngles.y += Input.GetAxis("Mouse X") * _speed;
+        }
+        Quaternion newRotation = Quaternion.Euler(-_orbitAngles.x, _orbitAngles.y, 0f);
+        Vector3 offset = newRotation * new Vector3(0f, 0f, _distance);
+        transform.position = _target.position + offset;
+        transform.rotation = newRotation;
+
+        // Mouse wheel to zoom in and out
+        _distance += Input.GetAxis("Mouse ScrollWheel") * _speed * 1.5f;
+        _distance = Mathf.Clamp(_distance, -20, -10);
+    }
+}
